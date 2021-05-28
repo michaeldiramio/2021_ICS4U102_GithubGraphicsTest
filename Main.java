529967c0b502982bfdd97b002d7b2d5a2f8dc027
Doubrouc <>
2021-05-28 14:53:03 +0000

    Readd ut

---
+++ Main.java
@@ -0,0 +1,44 @@
+import DLibX.*;
+import MiniGames.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    DConsole dc = new DConsole(600,400);
+    dc.setOrigin(DConsole.ORIGIN_CENTER);
+    Random r = new Random();
+
+    ArrayList<MiniGame> games = new ArrayList<>();
+    games.add(new DiRamioGame());
+
+    // play games FOREVER!
+    while(true) {
+
+      // pre-game screen
+      dc.clear();
+      dc.drawString("Press Space to Play Next Game", dc.getWidth() / 2, dc.getHeight() / 2);
+      dc.redraw();
+
+      while(!dc.isKeyPressed(' ')) { // wait until they press space
+        dc.pause(20);
+      }
+
+      MiniGame toPlay = games.get(r.nextInt(games.size())); // randomly choose a game
+      int score = toPlay.playGame(dc); // play the game
+
+      // display how they did in the current game
+      dc.clear();
+      dc.drawString("You got " + score, dc.getWidth() / 2, dc.getHeight() / 2);
+      dc.redraw();
+
+      dc.pause(2000);
+    }
+
+
+  }
+
+
+}
